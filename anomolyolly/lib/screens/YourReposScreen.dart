25b3b1622acfa0fd0a55fb0a0312312dade02a25
cwarren038 <callie.warren@cbsinteractive.com>
2022-06-28 16:36:16 -0500

    add search bar
    Signed-off-by: cwarren038 <callie.warren@cbsinteractive.com>

--- anomolyolly/lib/screens/YourReposScreen.dart
+++ anomolyolly/lib/screens/YourReposScreen.dart
@@ -16,6 +16,7 @@ class YourReposScreen extends StatefulWidget {
 
 class _YourReposState extends State<YourReposScreen> {
   late Future<List<Repo>> futureRepos;
+  String searchString = "";
 
   @override
   void initState() {
@@ -27,25 +28,49 @@ class _YourReposState extends State<YourReposScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: FutureBuilder<List<Repo>>(
-          future: futureRepos,
-          builder: (context, snapshot) {
-            List<Widget>? repositoryRows;
-            if(snapshot.hasData) {
-              repositoryRows = getRepositoryRows(snapshot.data);
-            }
-            if (repositoryRows != null) {
-              return ListView(children: repositoryRows);
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    searchString = value.toLowerCase();
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  suffixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: FutureBuilder<List<Repo>>(
+                  future: futureRepos,
+                  builder: (context, snapshot) {
+                    List<Widget>? repositoryRows;
+                    if (snapshot.hasData) {
+                      repositoryRows = getRepositoryRows(snapshot.data);
+                    }
+                    if (repositoryRows != null) {
+                      return ListView(children: repositoryRows);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-            }
-        ),
-        ),
+                    // By default, show a loading spinner.
+                    return const CircularProgressIndicator();
+                  }
+              ),
+            )
+
+          ]
+      ),
     );
   }
 
